d0a14434738a813bd90473fc352019f37f70718a
Alice Shrestha <alice_shrestha@hotmail.com>
2016-01-03 22:19:18 +0000

    With the nav tabs

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 //Initialize size of the window
 var windowSize = $(window).height();
-var currentPosition,delayWheel;
+var currentPosition,delayWheel = 0;
 
 window.onresize = function(){
 	windowSize = $(window).height();
 	window.scrollTo(0,windowSize*currentPosition);
+	$("#page").css({"height":windowSize})
 }
 
 $(function(){
-	$('body').bind('mousewheel DOMMouseScroll', function (event) {      
-		 event.preventDefault();     
-		 if (delayWheel == false)
-		 {
-		 	return;
-		 }
-		 delayWheel = false;
-	     if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-	     	  scroll("up");
-	     }
-	     else {
-	     	  scroll("down");
-	     }
+
+	window.scrollTo(0,0);
+
+	$('body').bind('keydown mousewheel DOMMouseScroll', function (event) {  
+
+			 if (event.originalEvent.wheenDelta){event.preventDefault();}
+
+			 var currTime = new Date().getTime();  
+			 if (delayWheel+1200 > currTime)
+			 {
+			 	return;
+			 }
+
+			 delayWheel = currTime;
+
+		     if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0 || event.which == 38) {
+		     	  if (event.which == 38) {delayWheel = currTime-1200;}
+		     	  scroll("up");
+		     }
+		     else if (event.which == 40 || event.which == 1){
+		     	  if (event.which == 40) {delayWheel = currTime-1200;}
+		     	  scroll("down");
+		     }
 	});
 })
 
@@ -28,7 +39,7 @@ function scroll(position)
 {
 	if (position=="up")
 		{
-			for (var i=1;i<=windowSize;i++)
+			for (var i=windowSize;i>=1;i--)
 			{
 				doSetTimeOut(-1,i);
 			}
@@ -49,7 +60,6 @@ function doSetTimeOut(chg,i)
 		if (windowSize==i)
 		{
 			currentPosition = $(document).scrollTop()/windowSize;
-			delayWheel = true;	
 		}
-	},0.7*i);
+	},0.4*i);
 }
